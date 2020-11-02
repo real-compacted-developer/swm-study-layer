6d26a842914ba7b91708c4ad6ee93b9c33081dcf
combbm <combbm@naver.com>
2020-11-02 20:43:48 +0900

    feat: add init code into StudyPeople store

--- src/stores/StudyPeople.ts
+++ src/stores/StudyPeople.ts
@@ -10,17 +10,29 @@ class StudyPeople {
   }
 
   join(room: RoomId, user: UserId) {
+    if (this.peopleList[room] === undefined) {
+      this.peopleList[room] = [];
+    }
+
     this.peopleList[room].push(user);
     return this.peopleList[room];
   }
 
   quit(room: RoomId, user: UserId) {
+    if (this.peopleList[room] === undefined) {
+      this.peopleList[room] = [];
+    }
+
     const index = this.peopleList[room].findIndex((v) => v === user);
     this.peopleList[room].splice(index, 1);
     return this.peopleList[room];
   }
 
   getPeople(room: RoomId) {
+    if (this.peopleList[room] === undefined) {
+      this.peopleList[room] = [];
+    }
+
     return this.peopleList[room];
   }
 
